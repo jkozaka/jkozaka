f75e506245b27f01e307579fced8e73030779d91
jkozaka <57858650+jkozaka@users.noreply.github.com>
2022-01-07 15:21:43 +0000

    bepsi

--- tests/colouredtxt.py
+++ tests/colouredtxt.py
@@ -1,5 +1,24 @@
-def esc(code):
-    return "\033["+ str(code) + "m"
-colours = [30 , 31 ,32 ,33 ,34 ,35 ,36 ,37 ,90 ,91 ,92 ,93 ,94 ,95 ,96, 97]
-for x in colours:
-    print(esc(str(x)) + "banana" + esc(0))
+import os
+import time
+class functions:
+    global esc
+    global square
+    global clear
+    clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+    def esc(code):
+        return "\033[" + str(code) + "m"
+class main:
+    while True:
+        print(esc(1) + esc(31))
+        clear()
+        print("/")
+        time.sleep(0.1)
+        clear()
+        print("-")
+        time.sleep(0.1)
+        clear()
+        print("\\")
+        time.sleep(0.1)
+        clear()
+        print("|")
+        time.sleep(0.1)
