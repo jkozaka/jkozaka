974582ef1fc368e826ea568856307b6a564fecb7
jkozaka <57858650+jkozaka@users.noreply.github.com>
2022-01-07 15:20:56 +0000

    asdasdasd

--- minesweeper/main.py
+++ minesweeper/main.py
@@ -1,17 +1,22 @@
 import random
-import time
+import os
 class functions:
     global esc
     global square
+    global clear
+    clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
     def esc(code):
         return "\033[" + str(code) + "m"
     def square(number, colour):
         colcod = colour + 40
         return esc(str(colcod)) + esc(4) + str(number) + "▐" + esc(0)
+class presqs:
+    global blanksq
+    global randsq
+    blanksq = square(" " ,7)
+    randsq = square(random.randint(0,9) , random.randint(1,6))
 for a in range(0,3):
     print(square(random.randint(0,9) , random.randint(1,6))
      + square(random.randint(0,9) , random.randint(1,6))
      + square(random.randint(0,9) , random.randint(1,6)))
-blanksq = print(square(" " ,7))
-blanksq
-time.sleep(1)
+print(blanksq)
