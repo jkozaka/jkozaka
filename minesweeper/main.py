2b3c9aba6c60ffb28a1e0af034ecb96131a2d192
jkozaka <57858650+jkozaka@users.noreply.github.com>
2022-01-08 13:49:20 +0000

    upgrade

--- minesweeper/main.py
+++ minesweeper/main.py
@@ -4,19 +4,15 @@ class functions:
     global esc
     global square
     global clear
-    clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-    def esc(code):
-        return "\033[" + str(code) + "m"
-    def square(number, colour):
-        colcod = colour + 40
-        return esc(str(colcod)) + esc(4) + str(number) + "▐" + esc(0)
-class presqs:
     global blanksq
     global randsq
+    clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+    esc = lambda code : "\033[" + str(code) + "m"
+    square = lambda number, colour: esc(str(colour + 40)) + esc(4) + str(number) + "▐" + esc(0)
     blanksq = square(" " ,7)
     randsq = square(random.randint(0,9) , random.randint(1,6))
-for a in range(0,3):
-    print(square(random.randint(0,9) , random.randint(1,6))
-     + square(random.randint(0,9) , random.randint(1,6))
-     + square(random.randint(0,9) , random.randint(1,6)))
-print(blanksq)
+class demominefield:
+    for a in range(0,3):
+        print(square(random.randint(0,9) , random.randint(1,6))
+         + square(random.randint(0,9) , random.randint(1,6))
+         + square(random.randint(0,9) , random.randint(1,6)))
