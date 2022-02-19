8d9f776bfdfca752a9d1e26c4960f1646120cdc7
jkozaka <57858650+jkozaka@users.noreply.github.com>
2022-02-19 19:00:19 +0000

    added archives + commented  and organized reminder script

--- productivity/reminder.py
+++ productivity/reminder.py
@@ -1,13 +1,24 @@
 #✓
+#todo: propery comment objects
+
 import os
-os.chdir("C:/Users/jonas/Documents/GitHub/jkozaka/productivity")
-clear = os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-remder = open("do_this.txt", "r").read()
-remders = remder.split("\n")
-remsplit = []
-expand = [1]
-clear
-for a in remders:
-    remsplit.append(a.split(":"))
-for b in range(0,len(remsplit)):
-    print(remsplit[b][0] if b in expand else remsplit[b][0] + " : " + remsplit[b][1])
+os.chdir("/run/media/jkozaka/jkozaka/github/jkozaka/productivity") #sets current directory
+
+clear = os.system('cls' if os.name in ('nt', 'dos') else 'clear')#defines the function to clear terminal
+
+def reload():
+    remder = open("do_this.txt", "r").read() #the contents of the reminder list
+    remders = remder.split("\n") #list of all different reminders. (expanded)
+    remsplit = [] #list of lists(lists consist of title and details )
+    expand = [] #list of reminders to expand (list ids)
+    #creates empty lists
+
+    for a in remders:
+        remsplit.append(a.split(":"))
+    #fills in remsplit
+
+    clear
+    
+    for b in range(0,len(remsplit)):
+        print(remsplit[b][0] + " : " + remsplit[b][1] if b in expand else remsplit[b][0])
+    #lists out todo list
