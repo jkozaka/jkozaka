661e5be36525cf936b464a23eedc1ed5537d825d
jkozaka <57858650+jkozaka@users.noreply.github.com>
2022-03-04 08:48:52 +0000

    dasas

--- productivity/reminder.py
+++ productivity/reminder.py
@@ -2,14 +2,15 @@
 import os
 os.chdir("/run/media/jkozaka/jkozaka/github/jkozaka/productivity") 
 
+expand = [] #list of reminders to expand (list ids)
+
 clear = os.system('cls' if os.name in ('nt', 'dos') else 'clear')#defines the function to clear terminal
 
 def reload():
     remder = open("do_this.txt", "r").read() #the contents of the reminder list
     remders = remder.split("\n") #list of all different reminders. (expanded)
     remsplit = [] #list of lists(lists consist of title and details )
-    expand = [] #list of reminders to expand (list ids)
-    #creates empty lists
+    
 
     for a in remders:
         remsplit.append(a.split(":"))
@@ -21,7 +22,18 @@ def reload():
         print(remsplit[b][0] + " : " + remsplit[b][2] if b in expand else remsplit[b][0])
     #lists out todo list
 
-def editprompt():
-    command = input(">>") #createst variable for command
+def prmpt():
+    def help():
 
-reload()
+        helpchs = input("available commands:\n1-add\n2-remove\n3-edit order\n4-edit reminders\n5-mark as done\n6-leave\n> ")
+        #selects choice
+
+        if helpchs == "6":
+            pass
+        
+
+    cmd = input(">> ") #createst variable for command
+    help()
+
+reload() #loads list
+prmpt() #prompts  for command
