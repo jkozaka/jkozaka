84901a9ff1f0697b4b12275e775eee734f5bcb66
jkozaka <57858650+jkozaka@users.noreply.github.com>
2022-07-08 18:14:01 +0100

    added tests

--- langs/c/playground.c
+++ langs/c/playground.c
@@ -1,11 +1,23 @@
-#include <sys/ioctl.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
+#include "jlib.h"
 int main() {
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    char ok=1;
+    while(ok){
+    char animal[5];
+    fgets(animal,3,stdin);
 
-    printf ("lines %d\n", w.ws_row);
-    printf ("columns %d\n", w.ws_col);
+    char lowered[5];
+    for(int x = 0;x<5;x++){
+        lowered[x] = lower(animal[x]);
+    }
+    if (strcmp(lowered, "dog\n")) {
+        puts("WOOF");
+    } else if (strcmp(lowered, "cat\n")) {
+        puts("MEOW");
+    } else {
+        ok=0;
+    }
+    }
     return 0;
 }
